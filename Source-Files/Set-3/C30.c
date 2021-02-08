c8d68c5274d2c9975ff0fb24567c0bb236bfef8c
KingDragon100 <kabirdeula167@gmail.com>
2021-02-08 21:57:05 +0545

    Value of base to power

---
+++ Source-Files/Set-3/C30.c
@@ -0,0 +1,15 @@
+//Program to find the value of given base to the given power. [y=a^b]
+
+#include<stdio.h>
+#include<math.h>
+
+void main(){
+    int base,power,value;
+    printf("Enter the base & power: ");
+    scanf("%d%d",&base,&power);
+    value = pow(base,power);
+    printf("The value of base(%d) raised to the power(%d) is %d\n",base,power,value);
+
+    //gcc S330.c -lm
+    //(./a.out)
+}
