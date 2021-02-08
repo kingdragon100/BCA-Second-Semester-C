d66b29c7d2727090461ddf3ec884c9a6c7435bb6
KingDragon100 <kabirdeula167@gmail.com>
2021-02-08 21:47:42 +0545

    10 Natural Numbers

---
+++ Source-Files/Set-3/C04.c
@@ -0,0 +1,14 @@
+//Program to generate first 10 natural numbers using do-while loop
+
+#include<stdio.h>
+
+void main(){
+    int i = 0;
+
+    do
+    {
+        printf("%d\n", i);
+        i++;
+    } while (i < 10);
+    
+}
