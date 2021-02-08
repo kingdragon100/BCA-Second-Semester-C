5e04159a0326b53730f1ef63aa5efbe0f4dacc31
KingDragon100 <kabirdeula167@gmail.com>
2021-02-08 21:55:29 +0545

    Display series

---
+++ Source-Files/Set-3/C23.c
@@ -0,0 +1,26 @@
+/*Write a program to generate the following output:
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+*/
+
+#include<stdio.h>
+
+void main(){
+  int n = 5;
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = n - 1; j >= i; j--)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
