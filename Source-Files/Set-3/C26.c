798d199066e8bf373a4144e6a9b8522413d00f24
KingDragon100 <kabirdeula167@gmail.com>
2021-02-08 21:55:55 +0545

    Pyramid

---
+++ Source-Files/Set-3/C26.c
@@ -0,0 +1,25 @@
+/*Write a program to generate the pyramid of number as
+   *
+  ***
+ *****
+*******
+*/
+
+#include<stdio.h>
+
+void main(){
+    int n = 4;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n-1; j >= i; j--)
+        {
+            printf(" ");
+        }
+        for (int k = 3; k <= i*2 + 1; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
