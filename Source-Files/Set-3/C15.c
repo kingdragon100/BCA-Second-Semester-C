1b97bf513e15421e6b04283968bb7909ffb5e0c3
KingDragon100 <kabirdeula167@gmail.com>
2021-02-08 21:52:29 +0545

    Prime numbers between 1 to 100

---
+++ Source-Files/Set-3/C15.c
@@ -0,0 +1,24 @@
+//Program to print all the prime numbers between 1 to 100.
+
+#include<stdio.h>
+
+void main(){
+    int i, j, prime;
+
+    for (i = 2; i < 100; i++)
+    {
+        prime = 1;
+        for (j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                prime = 0;
+                break;
+            }
+        }
+    if(prime){
+        printf("%4d",i);
+    }
+    }
+    printf("\n");
+}
