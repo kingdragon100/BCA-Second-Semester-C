9bf6497526872c242636b02755262914c329ea44
KingDragon100 <kabirdeula167@gmail.com>
2021-02-08 21:51:37 +0545

    Sum of digits

---
+++ Source-Files/Set-3/C12.c
@@ -0,0 +1,17 @@
+//Program to find the sum of digits of a given number.
+
+#include<stdio.h>
+
+void main(){
+    int sum = 0, n, rem;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        rem = n % 10;
+        n = n / 10;
+        sum += rem;
+    }
+    printf("Sum: %d\n", sum);
+}
