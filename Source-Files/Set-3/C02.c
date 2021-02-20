974f96605baf7bc6b75c0a3a57ec2fe9e4ed9b58
kingdragon100 <kabirdeula167@gmail.com>
2021-02-20 17:36:24 +0545

    modified

--- Source-Files/Set-3/C02.c
+++ Source-Files/Set-3/C02.c
@@ -4,14 +4,13 @@
 
 void main(){
     char name[25];
-    int i;
-    i = 1;
+    int i = 1;
     printf("Enter your name: ");
     scanf("%s", name);
 
     while (i <= 10)
     {
-        printf("Name: %2d. %s\n", i, name);
+        printf("%2d. %s\n", i, name);
         i++;
     }
     
